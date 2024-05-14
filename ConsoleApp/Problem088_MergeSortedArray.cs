8251a9a73bb45e2e09bce73cc5203be4f2424d58
Jose Tucci Jr <tucci.jr@gmail.com>
2024-05-14 19:28:27 -0300

    :art: Other ways

--- ConsoleApp/Problem088_MergeSortedArray.cs
+++ ConsoleApp/Problem088_MergeSortedArray.cs
@@ -3,14 +3,94 @@ internal class Problem088_MergeSortedArray
 {
     public Problem088_MergeSortedArray()
     {
-        Merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+        Merge([1,8,9, 0, 0, 0], 3, [2, 5, 6], 3);
+        //Merge([1, 2, 3, 0, 0, 0], 3, [1], 1);
+        //Merge([1], 1, [], 0);
+        //Merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
         //Merge([2, 0], 1, [1], 1);
         //Merge([4, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
-        //Merge([1], 1, [], 0);
         //Merge([0], 0, [1], 1);
     }
-
     public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int i = m + n - 1;
+
+        while (p2 >= 0)
+        {
+            var n1 = p1 >= 0 ? nums1[p1] : 0;
+            var n2 = nums2[p2];
+
+            if (p1 >= 0 && n1 > n2)
+                nums1[i--] = nums1[p1--];
+            else
+                nums1[i--] = nums2[p2--];
+        }
+
+        PrintArray(nums1);
+    }
+    public void Merge6(int[] nums1, int m, int[] nums2, int n)
+    {
+        var l = m - 1;
+        var r = n - 1;
+        var k = m + n - 1;
+
+        while (l >= 0 && r >= 0)
+        {
+            var n1 = l >= 0 ? nums1[l] : 0;
+            var n2 = nums2[r];
+
+            if (nums1[l] > nums2[r])
+            {
+                nums1[k] = nums1[l--];
+            }
+            else
+            {
+                nums1[k] = nums2[r--];
+            }
+            k--;
+        }
+
+        while (l >= 0)
+        {
+            nums1[k--] = nums1[l--];
+        }
+
+        while (r >= 0)
+        {
+            nums1[k--] = nums2[r--];
+        }
+
+        PrintArray(nums1);
+    }
+
+    public void Merge5(int[] nums1, int m, int[] nums2, int n)
+    {
+        var x1 = m - 1;
+        var x2 = n - 1;
+        var k = m + n - 1;
+
+        Console.WriteLine($"x2 - x1 - k - n1[x1] - n2[x2] -> n1[k]");
+        while (x2 >= 0)
+        {
+            Console.Write($"{x2}  - {x1}  - {k} - {(x1 >= 0 ? nums1[x1] : 0)}      - {nums2[x2]}");
+            if (x1 >= 0 && nums1[x1] > nums2[x2])
+            {
+                nums1[k--] = nums1[x1--];
+            }
+            else
+            {
+                nums1[k--] = nums2[x2--];
+            }
+
+            //Console.WriteLine($"      -> {nums1[k]}");
+        }
+
+        PrintArray(nums1);
+    }
+
+    public void Merge4(int[] nums1, int m, int[] nums2, int n)
     {
         var ptM = 0;
         var ptN = 0;
@@ -67,10 +147,11 @@ internal class Problem088_MergeSortedArray
     {
         Console.WriteLine($"Array: {string.Join(",", array)}");
 
-        for (int i = 0; i < array.Length; i++)
-        {
-            Console.WriteLine(array[i]);
-        }
+        //for (int i = 0; i < array.Length; i++)
+        //{
+        //    Console.WriteLine(array[i]);
+        //}
+
         Console.WriteLine("--- x ---");
     }
 
@@ -87,6 +168,13 @@ internal class Problem088_MergeSortedArray
 /*
 1: [1,2,3]
 2: [2,5,6]
+m = 3
+n = 3
+x1 = 2
+x2 = 2
+k = 5
 
+x2 - x1 - k - n1[x1] - n2[x2] -> n1[k]
+2  - 2  - 5 - 3      - 6      -> 6
 
 */
